e1621a5eb38fb2d59326675b746df03ab86ab864
e759ksh <e759ksh@naver.com>
2022-12-03 11:31:26 +0900

    logo add

---
+++ client/src/components/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img
+      src="../assets/sign.png"
+      alt=""
+      oncontextmenu="return false"
+      onselectstart="return false"
+    />
+    <div>13rwsf</div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {},
+};
+</script>
+
+<style>
+img {
+  user-select: none;
+}
+</style>
